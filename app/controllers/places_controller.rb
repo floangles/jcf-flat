5d613602b56ab9c1dcc614407c1e04cfc71ddc63
florent angles <florent.angles@hei.fr>
2015-06-22 19:17:36 +0200

    l'heure de partir

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,24 @@
+class PlacesController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
