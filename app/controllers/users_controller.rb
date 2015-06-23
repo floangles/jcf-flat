7fab27f7fa54c42a019f24ec62b13ab99fb4e606
christopher lemoine <ch_lemoine@yahoo.fr>
2015-06-23 14:37:54 +0200

    user paperclip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email,
-      :phone_number, :address, :zipcode, :city, :sex, :description)
+      :phone_number, :address, :zipcode, :city, :sex, :description, :picture)
   end
 end
