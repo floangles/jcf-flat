a6f76969873f3b3de307873300dfb5bdd26155b4
florent angles <florent.angles@hei.fr>
2015-06-24 11:22:34 +0200

    reset db

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,14 +1,15 @@
 class PlacesController < ApplicationController
-  before_action :set_place, only: [:show, :destroy, :edit, :update]
+
 
   def index
     @places = Place.where(city: params[:city])
   end
 
   def show
-    @Place = Place.find(params[:id])
-
+    @booking = Booking.new
+    @place = Place.find(params[:id])
   end
+
 
   private
 
