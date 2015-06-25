a69767593082c9016c506abed690692016521a1f
christopher lemoine <ch_lemoine@yahoo.fr>
2015-06-25 14:32:13 +0200

    buttons for approval OK

--- app/controllers/profile/rentings_controller.rb
+++ app/controllers/profile/rentings_controller.rb
@@ -6,6 +6,21 @@ module Profile
     def index
       @rentings = current_user.rentings
     end
+
+    def update
+      if params[:status] == "Approved"
+        @renting = current_user.rentings.find(params[:id])
+        @renting.update(status: "Approved")
+        redirect_to profile_rentings_path
+      elsif params[:status] == "Refused"
+        @renting = current_user.rentings.find(params[:id])
+        @renting.update(status: "Refused")
+        redirect_to profile_rentings_path
+      end
+
+      # @booking.flat.update() if you want to update the availability of the flat, for example
+    end
+
   end
 
 end
