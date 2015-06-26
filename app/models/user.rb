72635649243a4149921c8e8463af8cbb94581b71
florent angles <florent.angles@hei.fr>
2015-06-26 16:46:04 +0200

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  after_create :send_welcome_email
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -40,10 +40,7 @@ class User < ActiveRecord::Base
     end
   end
 
-  private
 
-  def send_welcome_email
-    UserMailer.welcome(self).deliver_now
-  end
+
 
 end
